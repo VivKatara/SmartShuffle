e8f37326dc5d29ef413da4bd68dd07828bb2571b
leaspark <lea.sparkman@yale.edu>
2019-11-21 12:35:46 -0500

    incporp johan/viv changes

--- routes.js
+++ routes.js
@@ -1,4 +1,3 @@
-
 // npm deps
 const express = require('express');
 const https = require('https');
@@ -8,8 +7,10 @@ const { URL } = require('url');
 const QueryString = require('querystring');
 const playlist_response = require('./sample_response.js')
 
+const {spClientId, spClientSecret, spBaseUrl, authToken, port} = require('./config');
+
 //Env vars
-const {spClientId, spClientSecret, spBaseUrl, port} = require('./config');
+//const {spClientId, spClientSecret, spBaseUrl, port} = require('./config');
 
 // Require the framework and instantiate it
 const app = express();
@@ -23,9 +24,7 @@ app.get('/', (req, res) => {
 });
 
 
-
 app.get('/getPlaylists', function (req, res) {
-
   console.log("trying to get some songs", req.query.token); 
   let request = "https://api.spotify.com/v1/me/playlists";
   let headers = 
@@ -35,7 +34,6 @@ app.get('/getPlaylists', function (req, res) {
       'Content-Length': '0'
       // 'Content-Type': 'application/x-www-form-urlencoded',
 }
-
 
    fetch(request, { 'headers': headers})
 	   .then(res => res.json())
@@ -50,9 +48,11 @@ app.get('/getPlaylists', function (req, res) {
 		  	trackData.images = tracks[i].images; 
 		  	response.push(trackData); 
   		}
-  		res.send(response);
+	   
+	   
+//  		res.send(response);
 
-	      console.log(data); 
+//	      console.log(data); 
 	   })
 	   .catch(err => {
 	      res.send(err);
@@ -60,15 +60,100 @@ app.get('/getPlaylists', function (req, res) {
  
   
   //do auth stuff and forward request
-
-
-
 })
+
+app.get('/smartshuffle', async (req, res) => {
+	let trackIds = []
+	let trackIdsWithVars = []
+	// const playlistId = req.playlistId
+	const playlistId = '4KK6ZMTlEeJ3B5A68YfU7V';
+//	const url = getUrl("https://api.spotify.com/v1//playlists/" + playlistId + "/tracks");
+	const url = "https://api.spotify.com/v1/playlists/" + playlistId + "/tracks";
+
+	let headers = 
+  	{
+    	'Authorization' : 'Bearer BQCaiSPbWAU3MQ8KT3PdteZYZpHfBefN6wnTOO-cHkgFEOXqlJCY2rjNLUFxT_XjjZg-ATwl_kT9jf6tm_JmQbvRqPGdd67CvQhXgkCqeWusXivPWrucU08Y8PgzGfEoazMOSw-3dC1al14IqKxp-S11QP1C8yhLqtwjUCmvFGDvg4mKt1HxlyIzMl1m6n8DVSOoiURadQ',
+      	'Content-Type': 'application/json',
+      	'Content-Length': '0'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+	}
+	
+	await fetch(url, {method: 'GET', headers: headers}).then(response => response.json())
+	.then(async (data) => {
+//		console.log(data);
+//		console.log(data.items);
+		let items = data["items"];
+//		console.log(items);
+		trackIds = getTrackIdsFromPlaylist(items);
+		console.log("track_ids:", trackIds);
+		trackIdsWithVars = await appendAudioAnalysis(trackIds);
+		console.log("with vars:", trackIdsWithVars);
+		
+		
+		let sorted_tracks = sort(trackIdsWithVars, 1);
+		console.log("sorted:", sorted_tracks);
+		res.send(JSON.stringify(sorted_tracks));
+
+//		res.send(trackIdsWithVars);
+	}).catch((err) => {
+		console.log(err);
+	})
+})
+
+function sort(tracks, index){
+	var sort_index = index;
+	var sorted_array = tracks.sort(function(a, b) {
+		return a[sort_index] > b[sort_index] ? 1: -1;
+	});
+	var song_array = [];
+	for (var i = 0; i < sorted_array.length;i++){
+		var song = sorted_array[i][0];
+		song_array.push(song);
+	}
+	return song_array;
+}
+
+async function appendAudioAnalysis(trackIds) {
+	let headers = 
+  	{
+    	'Authorization' : 'Bearer BQCaiSPbWAU3MQ8KT3PdteZYZpHfBefN6wnTOO-cHkgFEOXqlJCY2rjNLUFxT_XjjZg-ATwl_kT9jf6tm_JmQbvRqPGdd67CvQhXgkCqeWusXivPWrucU08Y8PgzGfEoazMOSw-3dC1al14IqKxp-S11QP1C8yhLqtwjUCmvFGDvg4mKt1HxlyIzMl1m6n8DVSOoiURadQ',
+      	'Content-Type': 'application/json',
+      	'Content-Length': '0'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+	}
+	let trackIdsWithVars = []
+	for (track in trackIds) {
+		const url = "https://api.spotify.com/v1/audio-analysis/" + trackIds[track];
+//		const url = getUrl("/audio-analysis/" + track);
+//		console.log(track)
+		await fetch(url, {method: 'GET', headers: headers}).then(response => response.json())
+		.then((data) => {
+//			console.log(data);
+			trackIdsWithVars.push([trackIds[track], data["track"]["tempo"]])
+		}).catch((err) => {
+			console.log(err)
+		})
+	}
+	return trackIdsWithVars
+}
+
+function getTrackIdsFromPlaylist(data) {
+	let trackIds = []
+	for (key in data) {
+		trackIds.push(data[key]["track"]["id"]);
+	}
+	return trackIds;
+}
+
+function getUrl(route) {
+	return spBaseUrl + route;''
+}
+
 
 // init spotify config
 const spClientCallback = process.env.SPOTIFY_CLIENT_CALLBACK;
 const authString = Buffer.from(spClientId+':'+spClientSecret).toString('base64');
-const authHeader = `Basic ${authString}`;
+const authHeader = Basic ${authString};
 const spotifyEndpoint = 'https://accounts.spotify.com/api/token';
 
 // encryption
@@ -242,5 +327,5 @@ app.post('/refresh', async (req, res) => {
 
 // start server
 app.listen(port || 3200, () => {
-	console.log(`Server is listening on port ${port}`);
+	console.log(Server is listening on port ${port});
 })
