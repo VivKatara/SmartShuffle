c879b55c60efbc1851bea641c147778a68a3798c
Max Model <maxmodel@Maxs-MacBook-Pro.local>
2019-12-11 22:56:42 -0500

    Sliders for all params

--- mobile/src/ShuffleParametersScreen.js
+++ mobile/src/ShuffleParametersScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Alert, Platform, StyleSheet, Text, View, SafeAreaView, Picker, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { Alert, ScrollView, Platform, StyleSheet, Text, View, SafeAreaView, Picker, TouchableOpacity, TouchableHighlight } from 'react-native';
 import { Dropdown } from 'react-native-material-dropdown';
 import { NavigationEvents } from 'react-navigation';
 import { ActivityIndicator, Avatar, IconButton, Appbar, Card } from 'react-native-paper';
@@ -17,6 +17,11 @@ export default class ShuffleParametersScreen extends Component {
       energy: 0.5,
       danceability: 0.5,
       instrumentalness: 0.5,
+      liveness: 0.5,
+      loudness: 0.5,
+      speechiness: 0.5,
+      valence: 0.5,
+      acousticness: 0.5,
       pickerItems: [],
       genreName: "",
       showSpinner: false,
@@ -47,12 +52,17 @@ export default class ShuffleParametersScreen extends Component {
     let request = "https://frightful-barrow-37052.herokuapp.com/smartshuffle?"
       + "tempo=" + this.state.tempo * 10
       + "&danceability=" + this.state.danceability * 10
-      + "&loudness=" + this.state.instrumentalness * 10
+      + "&instrumentalness=" + this.state.instrumentalness * 10
       + "&peak=" + this.state.energy * 10
+      + "&liveness=" + this.state.liveness * 10
+      + "&loudness=" + this.state.loudness * 10
+      + "&speechiness=" + this.state.speechiness * 10
+      + "&valence=" + this.state.valence * 10
+      + "&acousticness=" + this.state.acousticness * 10
       + "&playlistId1=" + this.state.playlistObject[0].id
       + "&playlistId2=" + this.state.playlistObject[1].id
       + "&playlistId3=" + this.state.playlistObject[2].id
-      + "&token=" + global.accessToken;//"BQAbXo6mvZz-RqP7RXcSk7WEUe2zxApUwN2gZvgmhWSLiE5o6vPsXixY9xVf5witUncu37yM9ehnGv4GgirjI9eqT8d6V5m58JuKqcyDIMlSRHtdDnXg3BhOca4MLsRVHRAG5K4YomGpv6qrDGxRFb2E7xWOIFXqoGl4PMdvnEQMBCCJQACJhdpHzcYiGpqku4mQLYC78w";
+      + "&token=" + global.accessToken; //"BQAbXo6mvZz-RqP7RXcSk7WEUe2zxApUwN2gZvgmhWSLiE5o6vPsXixY9xVf5witUncu37yM9ehnGv4GgirjI9eqT8d6V5m58JuKqcyDIMlSRHtdDnXg3BhOca4MLsRVHRAG5K4YomGpv6qrDGxRFb2E7xWOIFXqoGl4PMdvnEQMBCCJQACJhdpHzcYiGpqku4mQLYC78w";
 
 
       this.setState({ showSpinner: true});
@@ -80,9 +90,10 @@ export default class ShuffleParametersScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{flex: 50, alignSelf:"stretch", justifyContent:"center"}}>
+        <View style={{height: 80, alignSelf:"stretch", justifyContent:"center"}}>
           <Text style={styles.maintenanceForm}>Shuffle Parameters: </Text>
         </View>
+        <ScrollView style={{ flex: 1, alignSelf: 'stretch' }} contentInsetAdjustmentBehavior="automatic">
         <View style={{ paddingLeft: 6, flex: 180, alignSelf: "stretch", justifyContent: "space-around" }}>
           <Text>Tempo: {this.state.tempo}</Text>
           <Slider
@@ -96,8 +107,7 @@ export default class ShuffleParametersScreen extends Component {
             value={Math.round(this.state.energy * 100) / 100}
             onValueChange={energy => this.setState({ energy: Math.round(energy * 100) / 100 })}
           />
-
-          <ActivityIndicator style={{ paddingBottom: 30, flex: 1, justifyContent: 'center' }} size={76} color="#1DB954" animating={this.state.showSpinner} />
+          {this.state.showSpinner && <ActivityIndicator style={{ paddingBottom: 0, flex: 1, justifyContent: 'center' }} size={76} color="#1DB954" animating={this.state.showSpinner} />}
           <Text>Danceability: {this.state.danceability}</Text>
           <Slider
             thumbTintColor="#1DB954"
@@ -110,20 +120,48 @@ export default class ShuffleParametersScreen extends Component {
             value={Math.round(this.state.instrumentalness * 100) / 100}
             onValueChange={instrumentalness => this.setState({ instrumentalness: Math.round(instrumentalness * 100) / 100 })}
           />
-          <Dropdown label='Genre' onChangeText={(itemValue, itemIndex) => this.categoryValueChange(itemValue)} data={this.state.pickerItems} />
+          <Text>Valence: {this.state.valence}</Text>
+          <Slider
+            thumbTintColor="#1DB954"
+            value={Math.round(this.state.valence * 100) / 100}
+            onValueChange={valence => this.setState({ valence: Math.round(valence * 100) / 100 })}
+          />
+          <Text>Speechiness: {this.state.speechiness}</Text>
+          <Slider
+            thumbTintColor="#1DB954"
+            value={Math.round(this.state.speechiness * 100) / 100}
+            onValueChange={speechiness => this.setState({ speechiness: Math.round(speechiness * 100) / 100 })}
+          />
+          <Text>Loudness: {this.state.loudness}</Text>
+          <Slider
+            thumbTintColor="#1DB954"
+            value={Math.round(this.state.loudness * 100) / 100}
+            onValueChange={loudness => this.setState({ loudness: Math.round(loudness * 100) / 100 })}
+          />
+          <Text>Liveness: {this.state.liveness}</Text>
+          <Slider
+            thumbTintColor="#1DB954"
+            value={Math.round(this.state.liveness * 100) / 100}
+            onValueChange={liveness => this.setState({ liveness: Math.round(liveness * 100) / 100 })}
+          />
+          <Text>Acousticness: {this.state.acousticness}</Text>
+          <Slider
+            thumbTintColor="#1DB954"
+            value={Math.round(this.state.acousticness * 100) / 100}
+            onValueChange={acousticness => this.setState({ acousticness: Math.round(acousticness * 100) / 100 })}
+          />
           <View style={{ paddingLeft: 6, height: 30, alignSelf: "stretch", justifyContent: "space-around" }}></View>
         </View>
-        <View style={{ flex: 70, alignSelf: "stretch", justifyContent: "center", alignItems: "center" }}>
+        </ScrollView>
+        <View style={{ height: 150, alignSelf: "stretch", justifyContent: "center", alignItems: "center" }}>
             <View style={{flex: 50, alignSelf:"stretch", justifyContent: 'center', alignItems: 'center'}}>
-                <TouchableOpacity  style={styles.submitButton} disabled={!(this.state.genreName !== "")} onPress={this.getSortedPlaylist}>
+                <TouchableOpacity  style={styles.submitButton} onPress={this.getSortedPlaylist}>
                 <View style={{flex: 1, alignSelf:"stretch", justifyContent: 'center', alignItems: 'center'}}>
                     <Text style = {styles.buttontext}>SHUFFLE</Text>
                 </View>
               </TouchableOpacity>
             </View>
-            <View style={{flex: 1, alignSelf:"stretch", justifyContent: 'center', alignItems: 'center'}}>
-            </View>
-            <View style={{flex: 50, alignSelf:"stretch", justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{flex: 20, alignSelf:"stretch", justifyContent: 'center', alignItems: 'center'}}>
                 <TouchableHighlight onPress={this.spotifyLogoutButtonWasPressed}>
                     <Text>Logout</Text>
                 </TouchableHighlight>
