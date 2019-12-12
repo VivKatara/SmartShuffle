3381295dbffa8b55d4f806f778c0bdc91d1a67ef
leaspark <lea.sparkman@yale.edu>
2019-12-11 22:24:30 -0500

    changes to data viz -johans changes

--- data-viz/src/Data.js
+++ data-viz/src/Data.js
@@ -56,40 +56,32 @@ class Data extends React.Component {
 	constructor(props) {
 	  super(props);
 	  this.state = { 
-	  	playlists: [], 
-	  	artists: []
+	  	playlists: []
 	  };
-	  //this.handleClick = this.handleClick.bind(this);
 	}
 
 	componentDidMount() {
-//		console.log('hello world!'); 
 		db.collection("metadata")
 	    .get()
 	    .then(querySnapshot => {
 	      const data = querySnapshot.docs.map(doc => doc.data());
-			var average_song_length = ((data[2]["songminutes"]/1000)/data[2]["songCount"]).toFixed(2);
+			var average_song_length = ((data[2]["songminutes"]/1000)/data[2]["songCount"]);
+			var avg_minutes = (Math.floor(average_song_length / 60)).toString();
+			var avg_seconds = (Math.floor(average_song_length - avg_minutes * 60)).toString();
+
 			var average_popularity = (data[2]["popularity"]/data[2]["songCount"]).toFixed(2);
-			
+
 			var decades = data[0];
-			const newdata1 = [
+			const most_popular_years = [
   				["Year", "Playlists"]
 			];
 			
 			Object.keys(decades).forEach(function(key) {
-				newdata1.push([key, decades[key]]);
+				most_popular_years.push([key, decades[key]]);
 			});
 			
-			
-			const newdata2 = [
-  				["Decade", "Playlists"],
-				["1980", 23],
-				["1990", 43],
-				["2000", 18],
-				["2010", 32],
-			];
-			
-			const newdata3 = [
+			//this gets filled in loop below
+			const most_popular_decades = [
 				["Decade", "Songs"],
 				["< 1990's", 0],
 				["1990's", 0],
@@ -99,31 +91,29 @@ class Data extends React.Component {
 			]
 			Object.keys(decades).forEach(function(key) {
 				if ( Number(key) < 1990){
-					newdata3[1][1] += decades[key];
+					most_popular_decades[1][1] += decades[key];
 				} else if ( 1990 < Number(key) && Number(key) < 2000){
-					console.log("key:", key);
-					newdata3[2][1] += decades[key];
+					most_popular_decades[2][1] += decades[key];
 				} else if ( 2000 < Number(key) && Number(key) < 2010){
-					newdata3[3][1] += decades[key];
+					most_popular_decades[3][1] += decades[key];
 				} else if ( 2010 < Number(key) && Number(key) < 2020){
-					newdata3[4][1] += decades[key];
+					most_popular_decades[4][1] += decades[key];
 				} else if (Number(key) > 2020){
-					newdata3[5][1] += decades[key];
+					most_popular_decades[5][1] += decades[key];
 				}
 			});
 			
 			//prepare chart data
 	      this.setState({ 
 			  playlists: data,
-              both_types: false,
-              total_success_docs: 43,
-			  average_latency: 42,
 			  totalplaylists: data[2]["playlistcount"],
 			  totalsongs: data[2]["songCount"],
 			  average_song_length: average_song_length,
+			  avg_minutes: avg_minutes,
+			  avg_seconds: avg_seconds,
 			  average_popularity: average_popularity,
-			  newdata1: newdata1,
-			  newdata2: newdata3
+			  most_popular_years: most_popular_years,
+			  most_popular_decades: most_popular_decades
 	    });
 	})
 		
@@ -136,6 +126,7 @@ class Data extends React.Component {
 			for (var i=0; i<data.length; i++){
 				results.push([data[i]["list"]["name"], data[i]["list"]["images"][0]["url"]]);
 			}
+			//map scrollable playlists
 			const listItems = results.map((number) =>
 				<div style={{textAlignLast: "left"}}><img src={number[1]} style={{height: "40px", width: "40px"}}/> <a href={number[1]} style={{fontSize: "20px", color: "white"}} target="_blank">{number[0]}</a> <br /> <br /> </div>
 			);
@@ -148,6 +139,11 @@ class Data extends React.Component {
 	
 			  
    render() {
+	   if (!this.state.most_popular_years){
+		   return (
+			   <div></div>
+		   );
+	   } else {
       return (
 		  <div style={{marginTop: "-30px"}}>
 		  
@@ -164,7 +160,7 @@ class Data extends React.Component {
           				chartType="PieChart"
           				width="300px"
           				height="200px"
-          				data={this.state.newdata1}
+          				data={this.state.most_popular_years}
 		  				options={options1}
         			/>
                       
@@ -185,7 +181,7 @@ class Data extends React.Component {
           				chartType="PieChart"
           				width="300px"
           				height="200px"
-          				data={this.state.newdata2}
+          				data={this.state.most_popular_decades}
 		  				options={options2}
         			/>
                       
@@ -201,7 +197,7 @@ class Data extends React.Component {
                         </Container>
                     </Container>
 		  
-                    <Container className="card-value pt-4 text-x-large" style={{height: "200px", overflow: "scroll"}}>
+                    <Container className="card-value pt-4 text-x-large" style={{height: "200px", width: "350px", overflow: "scroll"}}>
 		  				 <ul>{this.state.listItems}</ul>
                     </Container>
                 </Container>
@@ -209,7 +205,6 @@ class Data extends React.Component {
 		  
 		  <br /><br />
 		  
-		  		  
   		<div id="graph" style={{display: "inline-block", boxShadow: "10px 10px 5px #162238"}}><Container style={{display: "inlineBlock"}}>
                 <Container className="card grid-card is-card-dark" style={{ margin: "55px" }} >
                     <Container className="card-heading">
@@ -219,11 +214,11 @@ class Data extends React.Component {
                     </Container>
 		  
                     <Container className="card-value pt-4 text-medium">
-		  				Total Songs Played
+		  				Total Songs Played:
 		  				<br /><br />
 		  				<Container className="card-value pt-4 text-x-large">
                         {this.state.totalsongs}
-                        <span className="text-large pr-1">songs</span>
+                        <span className="text-large pr-1"> songs</span>
 
                     </Container>
                       
@@ -231,8 +226,6 @@ class Data extends React.Component {
                 </Container>
             </Container></div>
 		  		  
-
-		  
   		<div id="graph" style={{display: "inline-block", boxShadow: "10px 10px 5px #162238"}}><Container style={{display: "inlineBlock"}}>
                 <Container className="card grid-card is-card-dark" style={{ margin: "55px" }} >
                     <Container className="card-heading">
@@ -245,17 +238,16 @@ class Data extends React.Component {
 		  				Average Song Length:
 		  				<br /><br />
 		  				<Container className="card-value pt-4 text-x-large">
-		  				{this.state.average_song_length}
-                        <span className="text-large pr-1">seconds</span>
+		  				{this.state.avg_minutes}
+                        <span className="text-large pr-1"> mins </span>
+		  				{this.state.avg_seconds}
+		  				<span className="text-large pr-1"> secs</span>
 
                     </Container>
-                      
                     </Container>
                 </Container>
             </Container></div>
 		  		  
-
-		  
   		<div id="graph" style={{display: "inline-block", boxShadow: "10px 10px 5px #162238"}}><Container style={{display: "inlineBlock"}}>
                 <Container className="card grid-card is-card-dark" style={{ margin: "55px" }} >
                     <Container className="card-heading">
@@ -269,10 +261,9 @@ class Data extends React.Component {
 		  				<br /><br />
 		  				<Container className="card-value pt-4 text-x-large">
                         {this.state.totalplaylists}
-                        <span className="text-large pr-1">playlists</span>
-
+                        <span className="text-large pr-1"> playlists</span>
                     </Container>
-                      
+		  
                     </Container>
                 </Container>
             </Container></div>
@@ -289,9 +280,8 @@ class Data extends React.Component {
 		  			Average Popularity:
 		  				<br /><br />
 		  				<Container className="card-value pt-4 text-x-large">
-                        {this.state.average_popularity}/30
-                        <span className="text-large pr-1">points</span>
-
+                        {this.state.average_popularity} / 100
+                        <span className="text-large pr-1"> points</span>
                     </Container>
                       
                     </Container>
@@ -299,6 +289,7 @@ class Data extends React.Component {
             </Container></div>
 		  </div>
       );
+   }
    }
 }
 
